7ba1a26ac1977c0d61ab2a4e85a260ad4970d8ee
Taeyoon Kim <partrita@users.noreply.github.com>
2022-07-08 15:22:43 +0900

    Update keymap.c

--- qmk_firmware/keyboards/ferris/keymaps/34key/keymap.c
+++ qmk_firmware/keyboards/ferris/keymaps/34key/keymap.c
@@ -1,3 +1,4 @@
+
 // this is the style you want to emulate.
 // This is the canonical layout file for the Quantum project. If you want to add another keyboard,
 
@@ -9,10 +10,10 @@
 // entirely and just use numbers.
 
 enum ferris_layers {
-  _Base,
-  _Navi,
-  _Numb,
-  _Func,
+    _Base,
+    _Navi,
+    _Numb,
+    _Func,
 };
 
 enum ferris_tap_dances {
@@ -23,17 +24,17 @@ enum ferris_tap_dances {
 };
 
 // Left-hand home row mods - Base Layer
-#define HOME_A LGUI_T(KC_A)
-#define HOME_S LALT_T(KC_S)
-#define HOME_D LCTL_T(KC_D)
-#define HOME_F LSFT_T(KC_F)
-#define HOME_J RSFT_T(KC_J)
-#define HOME_K LCTL_T(KC_K)
-#define HOME_L LALT_T(KC_L)
-#define HOME_QU LGUI_T(KC_QUOT)
+#define H_Z LGUI_T(KC_Z)
+#define H_X LALT_T(KC_X)
+#define H_C LCTL_T(KC_C)
+#define H_V LSFT_T(KC_V)
+#define H_M RSFT_T(KC_M)
+#define H_COM LCTL_T(KC_COMM)
+#define H_DOT LALT_T(KC_DOT)
+#define H_SLS LGUI_T(KC_SLSH)
 
 // layer keys
-#define Lay_SPC LT(_Func,KC_SPC)
+#define Lay_SPC LT(_Func, KC_SPC)
 #define NAVI MO(_Navi)
 #define NUMB MO(_Numb)
 
@@ -49,48 +50,45 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_C] = ACTION_TAP_DANCE_DOUBLE(KC_C, COPY),
     [TD_V] = ACTION_TAP_DANCE_DOUBLE(KC_V, PASTE),
     [TD_X] = ACTION_TAP_DANCE_DOUBLE(KC_X, CUT),
-    [TD_Z] = ACTION_TAP_DANCE_DOUBLE(KC_Z, UNDO),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_Base] = LAYOUT( /* QWERTY */
-    KC_Q,       KC_W,    KC_E,       KC_R,    KC_T,            KC_Y,    KC_U,     KC_I,      KC_O,          KC_P,
-    HOME_A,    HOME_S,   HOME_D,   HOME_F,    KC_G,            KC_H,    HOME_J,  HOME_K,    HOME_L,      HOME_QU,
-    TD(TD_Z), TD(TD_X), TD(TD_C), TD(TD_V),   KC_B,            KC_N,    KC_M,   KC_COMM,    KC_DOT,      KC_SLSH,
-                                     NAVI, Lay_SPC,         KC_BSPC,       NUMB
-  ),
-
+  [_Base] = LAYOUT(
+    KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
+    KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_QUOT,
+    H_Z, H_X, H_C, H_V, KC_B, KC_N, H_M, H_COM, H_DOT, H_SLS,
+    NAVI, Lay_SPC, KC_BSPC, NUMB),
+  
   [_Navi] = LAYOUT(
-      KC_WH_L,    KC_WH_D,   KC_WH_U,        KC_WH_R,        KC_ESC,           KC_DEL,   KC_HOME,  KC_PGDN,   KC_PGUP,      KC_END,
-      HOME_A,     HOME_S,    HOME_D,         KC_LSFT,        KC_TAB,           KC_ENT,    KC_RSFT,   HOME_K,    HOME_L,     HOME_QU,
-      KC_MS_L,    KC_MS_D,   KC_MS_U,        KC_MS_R,       KC_BTN1,          KC_BTN2,   KC_LEFT,  KC_DOWN,     KC_UP,     KC_RGHT,    
-                                    KC_NO,       Lay_SPC,                 KC_BSPC,       NUMB    
-    ),
+    KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_ESC, KC_DEL, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
+    KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_TAB, KC_ENT, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,
+    KC_Z, TD(TD_X), TD(TD_C), TD(TD_V), KC_BTN1, KC_BTN2, H_M, H_COM, H_DOT, H_SLS,
+    KC_NO, Lay_SPC, KC_BSPC, NUMB),
+  
   [_Numb] = LAYOUT(
-      KC_LBRC,        KC_7,     KC_8,          KC_9,        KC_RBRC,          KC_DEL,      KC_NO,     KC_NO,     KC_NO,       KC_NO,
-      KC_SCLN,        KC_4,     KC_5,          KC_6,         KC_EQL,          KC_ENT,     KC_RSFT,    HOME_K,    HOME_L,     HOME_QU,
-       KC_GRV,        KC_1,     KC_2,          KC_3,        KC_BSLS,           KC_NO,      KC_NO,     KC_NO,   KC_HAEN,       KC_NO,
-                                     KC_0,        KC_MINS,              KC_BSPC,       KC_NO
-      ),
+    KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC, KC_DEL, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_SCLN, KC_4, KC_5, KC_6, KC_EQL, KC_ENT, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_GRV, KC_1, KC_2, KC_3, KC_BSLS, KC_NO, H_M, H_COM, KC_HAEN, H_SLS,
+    KC_0, KC_MINS, KC_BSPC, KC_NO),
+                                                              
   [_Func] = LAYOUT(
-      KC_F12,     KC_F7,     KC_F8,           KC_F9,       KC_PSCR,            RESET,     DT_PRNT,   DT_DOWN,     DT_UP,     KC_NO,
-      KC_F11,     KC_F4,     KC_F5,           KC_F6,       KC_SCRL,          KC_CAPS,     KC_RSFT,    HOME_K,    HOME_L,   HOME_QU,
-      KC_F10,     KC_F1,     KC_F2,           KC_F3,       KC_PAUS,           KC_INS,      KC_NO,     KC_NO,   KC_HAEN,     KC_NO,
-                                     KC_NO,       Lay_SPC,            KC_BSPC,      KC_NO
-      )
-};
+    KC_F12, KC_F7, KC_F8, KC_F9, KC_PSCR, RESET, DT_PRNT, DT_DOWN, DT_UP, KC_NO,
+    KC_F11, KC_F4, KC_F5, KC_F6, KC_SCRL, KC_CAPS, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_F10, KC_F1, KC_F2, KC_F3, KC_PAUS, KC_INS, H_M, H_COM, KC_HAEN, H_SLS,
+    KC_NO, Lay_SPC, KC_BSPC, KC_NO),
+  };
 
 // modify `config.h` file
 // by adding #define COMBO_COUNT 1 (replacing 1 with the number that you’re using).
 // modify `rules.mk` file
-// by adding # COMBO_ENABLE = yes 
+// by adding # COMBO_ENABLE = yes
 
 enum combos {
-  SFT_HAN,
+    KOR,
 };
 
-const uint16_t PROGMEM sft_han_combo[] = {LSFT_T(KC_F), LT(_Func,KC_SPC), COMBO_END};
+const uint16_t PROGMEM kor[] = {KC_BSPC, LT(_Func, KC_SPC), COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
-  [SFT_HAN] = COMBO(sft_han_combo, KC_HAEN),
+    [KOR] = COMBO(kor, KC_HAEN),
 };
