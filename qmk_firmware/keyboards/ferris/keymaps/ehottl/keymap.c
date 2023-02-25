8c5b06341d0415fe92f04cef43ed240ed28cff64
Taeyoon Kim <partrita@gmail.com>
2023-02-25 00:04:58 +0000

    2023-02-25

--- qmk_firmware/keyboards/ferris/keymaps/ehottl/keymap.c
+++ qmk_firmware/keyboards/ferris/keymaps/ehottl/keymap.c
@@ -13,14 +13,14 @@ enum tap_dance_codes {
 };
 
 // Left-hand home row mods - Base Layer
-#define H_Z LGUI_T(KC_Z)
-#define H_X LALT_T(KC_X)
-#define H_C LCTL_T(KC_C)
-#define H_V LSFT_T(KC_V)
-#define H_M RSFT_T(KC_M)
-#define H_COM LCTL_T(KC_COMM)
-#define H_DOT LALT_T(KC_DOT)
-#define H_SLS LGUI_T(KC_SLSH)
+#define H_Z LSFT_T(KC_Z)
+#define H_X LCTL_T(KC_X)
+#define H_C LGUI_T(KC_C)
+#define H_V LALT_T(KC_V)
+#define H_M  LALT_T(KC_M)
+#define H_COM  LGUI_T(KC_COMM)
+#define H_DOT  LCTL_T(KC_DOT)
+#define H_SLS  LSFT_T(KC_SLSH)
 
 // layer keys
 #define FUNC LT(Func, KC_SPC)
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // ---------------------------------------------              ----------------------------------------------
         KC_A, KC_S, KC_D, KC_F, KC_G,                                 KC_H, KC_J, KC_K, KC_L, KC_QUOT,
         // ---------------------------------------------              ----------------------------------------------
-        H_Z, H_X, TD(DANCE_02), H_V, KC_B,                            KC_N, H_M, H_COM, H_DOT, H_SLS, 
+        H_Z, H_X, H_C, H_V, KC_B,                                     KC_N, H_M, H_COM, H_DOT, H_SLS, 
         // ---------------------------------------------              ----------------------------------------------
                                           NAVI, FUNC,                 KC_BSPC, NUMB
         // ---------------------------------------------              ----------------------------------------------
@@ -48,11 +48,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     
     [Navi] = LAYOUT(
         // ---------------------------------------------              ----------------------------------------------
-        KC_TAB, KC_WH_U, KC_MS_U, KC_WH_D, KC_ESC,                    KC_DEL, KC_HOME, KC_BTN3, KC_PGUP, KC_END,
+        KC_TAB, KC_WH_U, KC_MS_U, KC_WH_D, KC_ESC,                    KC_DEL, KC_HOME, KC_BTN3, KC_END, KC_NO,
         // ---------------------------------------------              ----------------------------------------------
-        KC_LCTL, KC_MS_L, KC_MS_D, KC_MS_R, KC_TAB,                   KC_ENT, KC_BTN1, KC_NO, KC_BTN2, KC_RGHT,
+        KC_LCTL, KC_MS_L, KC_MS_D, KC_MS_R, KC_TAB,                   KC_ENT, KC_BTN1, KC_NO, KC_BTN2, KC_NO,
         // ---------------------------------------------              ----------------------------------------------
-        KC_LSFT,  KC_NO, KC_NO, KC_NO, KC_NO,                         KC_NO, H_M, H_COM, H_DOT, H_SLS,
+        KC_LSFT,  KC_CUT, KC_COPY, KC_PASTE, KC_NO,                   KC_NO, KC_RALT, KC_LGUI, KC_LCTL, KC_LSFT,
         // ---------------------------------------------              ----------------------------------------------
                                          KC_NO, FUNC,                 KC_BSPC, KC_HAEN
         // ---------------------------------------------              ----------------------------------------------
@@ -60,11 +60,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [Numb] = LAYOUT(
         // ---------------------------------------------              ----------------------------------------------
-        KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC,                           KC_DEL, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC,                           KC_DEL, KC_HOME, KC_UP, KC_END, KC_PGUP,
         // ---------------------------------------------              ----------------------------------------------
-        KC_SCLN, KC_4, KC_5, KC_6, KC_EQL,                            KC_ENT, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_SCLN, KC_4, KC_5, KC_6, KC_EQL,                            KC_ENT, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,
         // ---------------------------------------------              ----------------------------------------------
-        KC_GRV, KC_1, KC_2, KC_3, KC_BSLS,                            KC_NO, H_M, H_COM, H_DOT, H_SLS,
+        KC_GRV, KC_1, KC_2, KC_3, KC_BSLS,                            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         // ---------------------------------------------              ----------------------------------------------
                                          KC_0, KC_MINS,               KC_BSPC, KC_NO
         // ---------------------------------------------              ----------------------------------------------
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // ---------------------------------------------              ----------------------------------------------
         KC_F11, KC_F4, KC_F5, KC_F6, KC_SCRL,                         KC_CAPS, KC_NO, KC_NO, KC_NO, KC_NO, 
         // ---------------------------------------------              ----------------------------------------------
-        KC_F10, KC_F1, KC_F2, KC_F3, KC_PAUS,                         KC_INS, H_M, H_COM, H_DOT, H_SLS, 
+        KC_F10, KC_F1, KC_F2, KC_F3, KC_PAUS,                         KC_INS, KC_NO, KC_NO, KC_NO, KC_NO,
         // ---------------------------------------------              ----------------------------------------------
                                          KC_NO, FUNC,                 KC_BSPC, KC_HAEN
         // ---------------------------------------------              ----------------------------------------------
